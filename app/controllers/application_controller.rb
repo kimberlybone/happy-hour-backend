626e0c588134530e49740b3fcbd178a4f957b170
github username <github email address>
2019-10-17 17:23:57 -0400

    authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::API
+
+  def authentication_json(user_id)
+    {token: make_token(user_id), user_id: user_id}
+  end
+
+  def make_token(user_id)
+    payload = {user_id: user_id}
+    JWT.encode(payload, hmac_secret, 'HS256')
+  end
+
+  def logged_in_user_id
+    token = request.headers["Authorization"]
+
+    begin
+      decoded_payload = JWT.decode(token, hmac_secret, true, {algorithm: 'HS256'})
+      return decoded_payload.first["user_id"].to_i
+    rescue
+      return nil
+    end
+  end
+
+  def logged_in?
+    !!logged_in_user_id
+  end
+
+  def hmac_secret
+    ENV["JWT_SECRET_KEY"]
+  end
+
 end
