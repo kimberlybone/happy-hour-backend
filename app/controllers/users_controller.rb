eab518eb7c958d74e7b6c22e56e57c4dc59cc9de
mrwerner392 <mrwerner392@gmail.com>
2019-10-21 10:11:58 -0400

    signup goes to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
-      render json: authentication_json(user.id)
+      render json: user
     else
       render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
     end
