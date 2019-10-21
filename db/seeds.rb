21c951e9dca2ea5b9c8852151f8de6b8a94bd3ce
kimberlybone <k_bone@uncg.edu>
2019-10-21 11:34:59 -0400

    Created Favorites in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 User.destroy_all
 Recipe.destroy_all
 Ingredient.destroy_all
 
+# USERS
 kim = User.create(name: "Kimberly", password: "1", age: 22, budget: 300)
 matt = User.create(name: "Matt", password: "123", age: 27, budget: 25)
 happyhour = User.create(name: "Happy Hour", password: "bar", age: 0, budget: 1000)
 
-margarita = Recipe.create(category: 'Margarita', name: 'Strawberry Margarita', price: 8, user: kim, instructions: "Mix")
 
+# RECIPES
+margarita = Recipe.create(category: 'Margarita', name: 'Strawberry Margarita', price: 8, user: kim, instructions: "Mix")
 mojito = Recipe.create(category: 'Mojito', name: 'Strawberry Mojito', price: 7, user: matt, instructions: "Add alcohol")
+
 
 
 ingredient_json = RestClient.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
@@ -56,6 +53,18 @@ recipes.each do |recipe|
   end
 end
 
+# FAVORITES
+Favorite.create(user: kim, recipe: Recipe.first)
+Favorite.create(user: kim, recipe: Recipe.second)
+Favorite.create(user: kim, recipe: Recipe.third)
+Favorite.create(user: kim, recipe: Recipe.fourth)
+Favorite.create(user: kim, recipe: Recipe.fifth)
+
+Favorite.create(user: matt, recipe: Recipe.all[5])
+Favorite.create(user: matt, recipe: Recipe.all[6])
+Favorite.create(user: matt, recipe: Recipe.all[7])
+Favorite.create(user: matt, recipe: Recipe.all[8])
+Favorite.create(user: matt, recipe: Recipe.all[9])
 
 # byebug
 nil
