d6682bd134e5e1cecaa02f951dcf527872958b26
kimberlybone <k_bone@uncg.edu>
2019-10-22 13:17:42 -0400

    add user name to recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,7 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :category, :name, :price, :instructions
+  attributes :id, :category, :name, :price, :instructions, :user_name
   has_many :ingredients
-
+  def user_name
+    self.object.user.name
+  end
 end
