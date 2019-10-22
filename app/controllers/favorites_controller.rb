e8e32e9d23225e235914c8b86aa459e87ea00f55
mrwerner392 <mrwerner392@gmail.com>
2019-10-22 10:22:09 -0400

    create favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,12 @@
 class FavoritesController < ApplicationController
+
+  def create
+    favorite = Favorite.create(recipe_id: params[:recipe_id], user_id: params[:user_id])
+    if favorite.valid?
+      render json: favorite
+    else
+      render json: {errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
 end
