bf4d09a0d0b0e12be89d1ec5997d3955562069f8
kimberlybone <k_bone@uncg.edu>
2019-10-26 03:13:37 -0400

    validations for signup added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
   # alias_attribute :drinks, :recipes
   has_many :recipes, dependent: :destroy
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, presence: true
+  validates :age, numericality: { greater_than: 20, message: "must be 21 or older. Try again next time. "}, presence: true
+  validates :budget, numericality: { less_than: 500, message: "must be less than $500. "}, presence: true
 
   has_secure_password
 end
